f6f5359ad10eeae45ae497b75ececbc95d2efafe
Admin <Admin@LAPTOP-7EGS00HA>
2023-10-09 18:46:30 +0700

    Add project files.

---
+++ CNPMNC1/Models/SanPham.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CNPMNC1.Models
+{
+    public class SanPham
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SanPham()
+        {
+            //this.OrderDetails = new HashSet<>();
+            ImagePro = "~/Content/images/1.jpg";
+        }
+
+        public string idsanpham { get; set; }
+        public string tensanpham { get; set; }
+        public string thuonghieu { get; set; }
+        public string namsx { get; set; }
+        public string mota { get; set; }
+        public string loai_sanpham { get; set; }
+        public Nullable<decimal> gia { get; set; }
+        public string ImagePro { get; set; }
+        [NotMapped]
+        public HttpPostedFileBase UploadImages { get; set; }
+    }
+}
