303ad2d9d9a3fad1e801aa357b0b3062e340d49f
xooithitj1903 <xooithitj1903@gmail.com>
2023-10-09 22:48:10 +0700

    Test

---
+++ CNPMNC1/Controllers/DangkyDangnhapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPMNC1.Controllers
+{
+    public class DangkyDangnhapController : Controller
+    {
+        // GET: DangkyDangnhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
